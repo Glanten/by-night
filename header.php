1656dfae4853889c3b22036301fde443c4d2e10c
Glanten <tern.glanten@gmail.com>
2024-10-21 15:43:10 +0100

    Prototype drop-down navbar.

--- header.php
+++ header.php
@@ -1,10 +1,13 @@
 <body>
-    <header>
-        <div class="header_link"><a href="/index.php">Home</a></div>
-        <div class="header_link"><a href="/clans.php">Clans</a></div>
-        <div class="header_link"><a href="/disciplines.php">Disciplines</a></div>
-        <div class="header_link"><a href="/hunger.php">Hunger</a></div>
-        <div class="header_link"><a href="/dice.php">Dice</a></div>
-        <div class="header_link"><a href="/about.php">About</a></div>
-    </header>
+    <button class="accordion">Navigate</button>
+    <div class="panel">
+        <header>
+            <div class="header_link"><a href="/index.php">Home</a></div>
+            <div class="header_link"><a href="/clans.php">Clans</a></div>
+            <div class="header_link"><a href="/disciplines.php">Disciplines</a></div>
+            <div class="header_link"><a href="/hunger.php">Hunger</a></div>
+            <div class="header_link"><a href="/dice.php">Dice</a></div>
+            <div class="header_link"><a href="/about.php">About</a></div>
+        </header>
+    </div>
     <main>
