00cbf89719b34b18d1c9fb7a6026dba910c20848
Glanten <tern.glanten@gmail.com>
2024-10-22 21:12:40 +0100

    Added content to the index for character creation.

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php include 'head.php';?>
 <?php include 'header.php';?>
 <?php include 'content_index.html';?>
+<?php include 'chargen/relationship_map.html';?>
+<?php include 'chargen/attributes.html';?>
+<?php include 'chargen/skills.html';?>
+<?php include 'chargen/beliefs.html';?>
+<?php include 'chargen/vampire.html';?>
+<?php include 'chargen/background.html';?>
+<?php include 'chargen/coterie.html';?>
 <?php include 'footer.php';?>
