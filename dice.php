6203c25a7c98bebccbe3782dfe134c3f774da6b1
Glanten <tern.glanten@gmail.com>
2024-10-23 18:27:30 +0100

    Finished Dice page. Need to replace Hunger page.

--- dice.php
+++ dice.php
@@ -1,4 +1,7 @@
 <?php include 'head.php';?>
 <?php include 'header.php';?>
+<div class="background_ankh">
+    <img src="images/vampireankh.webp" class="backdrop_image" />
+</div>
 <?php include 'content_dice.html';?>
 <?php include 'footer.php';?>
