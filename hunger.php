489143f1c9acfcb042146f8e5231bd53ed1aa245
Glanten <tern.glanten@gmail.com>
2024-10-31 07:30:18 +0000

    Structured traits page.

--- hunger.php
+++
@@ -1,4 +0,0 @@
-<?php include 'head.php';?>
-<?php include 'header.php';?>
-<?php include 'content_hunger.html';?>
-<?php include 'footer.php';?>
---
+++ traits.php
@@ -0,0 +1,15 @@
+<?php include 'head.php';?>
+<?php include 'header.php';?>
+<div class="background_ankh">
+    <img src="images/vampireankh.webp" class="backdrop_image" />
+</div>
+<div class="index_container">
+    <?php include 'traits/attributes.html';?>
+    <?php include 'traits/skills.html';?>
+    <?php include 'traits/convictions.html';?>
+    <?php include 'traits/bloodpotency.html';?>
+    <?php include 'traits/advantages.html';?>
+    <?php include 'traits/tenets.html';?>
+</div>
+<script src="index.js"></script>
+<?php include 'footer.php';?>
